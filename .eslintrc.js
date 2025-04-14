78a845d33a5336dca91385f7d503247b7c377384
Rafinhaa <rafinhaa@me.com>
2025-04-14 17:00:57 -0300

    chore: code style

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier", "unused-imports"],
+  rules: {
+    "prettier/prettier": "error",
+    "no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
