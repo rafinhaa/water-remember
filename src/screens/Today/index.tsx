51db93561de9008fa7c1392b72354cea63b4dfc8
Rafinhaa <rafinhaa@me.com>
2025-04-25 10:31:58 -0300

    feat: cup rive animation

--- src/screens/Today/index.tsx
+++ src/screens/Today/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "@tamagui/core"
+import { View } from "@tamagui/core"
+import { useRef } from "react"
+import { TouchableOpacity } from "react-native"
+import Rive, { RiveRef } from "rive-react-native"
+
+import { Typography } from "@/components"
 
 export const Today = () => {
+  const riveAnimationRef = useRef<RiveRef>(null)
+
+  const handleChangeAmount = () => {
+    riveAnimationRef.current?.setInputState("default", "input", 30)
+  }
+
   return (
-    <View>
-      <Text>Today</Text>
+    <View flex={1} bg="$sky500">
+      <TouchableOpacity onPress={handleChangeAmount}>
+        <Typography.Body>Change amount</Typography.Body>
+      </TouchableOpacity>
+      <Rive
+        ref={riveAnimationRef}
+        resourceName="water_cup"
+        style={{
+          flex: 1,
+        }}
+      />
     </View>
   )
 }
