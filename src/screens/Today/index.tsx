b0469f1c6dc1780a2d347778892c9c16ffaec136
Rafinhaa <rafinhaa@me.com>
2025-04-25 12:42:48 -0300

    feat: linear gradient

--- src/screens/Today/index.tsx
+++ src/screens/Today/index.tsx
@@ -1,15 +1,18 @@
-import { View } from "@tamagui/core"
+import { getTokens, View } from "@tamagui/core"
+import { LinearGradient } from "expo-linear-gradient"
 import { useRef } from "react"
 import { TouchableOpacity, useWindowDimensions } from "react-native"
-import Rive, { RiveRef } from "rive-react-native"
+import { RiveRef } from "rive-react-native"
 
 import { StyledSafeAreaView, Typography } from "@/components"
 import { useCustomTabBarHeight } from "@/hooks/useCustomTabBarHeight"
 
 export const Today = () => {
-  const riveAnimationRef = useRef<RiveRef>(null)
   const { distanceToBottom } = useCustomTabBarHeight()
   const dimensions = useWindowDimensions()
+  const tokens = getTokens()
+
+  const riveAnimationRef = useRef<RiveRef>(null)
 
   const availableHeight = dimensions.height - distanceToBottom
 
@@ -18,20 +21,25 @@ export const Today = () => {
   }
 
   return (
-    <StyledSafeAreaView edges={["top"]} flex={1} bg="transparent">
-      <View height={availableHeight} bg={"$gray500"}>
-        <TouchableOpacity onPress={handleChangeAmount}>
-          <Typography.Body>Change amount</Typography.Body>
-        </TouchableOpacity>
-      </View>
+    <LinearGradient
+      style={{ flex: 1 }}
+      colors={[tokens.color.primary700.val, tokens.color.sky500.val]}
+    >
+      <StyledSafeAreaView edges={["top"]} flex={1} bg="transparent">
+        <View height={availableHeight} bg={"transparent"}>
+          <TouchableOpacity onPress={handleChangeAmount}>
+            <Typography.Body>Change amount</Typography.Body>
+          </TouchableOpacity>
+        </View>
 
-      <Rive
+        {/* <Rive
         ref={riveAnimationRef}
         resourceName="water_cup"
         style={{
           flex: 1,
         }}
-      />
-    </StyledSafeAreaView>
+      /> */}
+      </StyledSafeAreaView>
+    </LinearGradient>
   )
 }
