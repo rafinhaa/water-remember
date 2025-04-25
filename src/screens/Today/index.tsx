9e63c6b3829ac92be91a034840f13a4fd54a112b
Rafinhaa <rafinhaa@me.com>
2025-04-25 15:00:24 -0300

    feat: screen Today

--- src/screens/Today/index.tsx
+++ src/screens/Today/index.tsx
@@ -1,11 +1,13 @@
 import { getTokens, View } from "@tamagui/core"
 import { LinearGradient } from "expo-linear-gradient"
 import { useRef } from "react"
-import { TouchableOpacity, useWindowDimensions } from "react-native"
-import { RiveRef } from "rive-react-native"
+import { useWindowDimensions } from "react-native"
+import Rive, { RiveRef } from "rive-react-native"
 
-import { StyledSafeAreaView, Typography } from "@/components"
+import { RNStyled, StyledSafeAreaView, Typography } from "@/components"
 import { useCustomTabBarHeight } from "@/hooks/useCustomTabBarHeight"
+
+import { InfoAmountDrunk } from "./components"
 
 export const Today = () => {
   const { distanceToBottom } = useCustomTabBarHeight()
@@ -26,19 +28,29 @@ export const Today = () => {
       colors={[tokens.color.primary700.val, tokens.color.sky500.val]}
     >
       <StyledSafeAreaView edges={["top"]} flex={1} bg="transparent">
-        <View height={availableHeight} bg={"transparent"}>
-          <TouchableOpacity onPress={handleChangeAmount}>
-            <Typography.Body>Change amount</Typography.Body>
-          </TouchableOpacity>
-        </View>
+        <View height={availableHeight} items="center" pb="$6" gap="$4">
+          <InfoAmountDrunk amountDrunk={100} totalAmount={50} />
 
-        {/* <Rive
-        ref={riveAnimationRef}
-        resourceName="water_cup"
-        style={{
-          flex: 1,
-        }}
-      /> */}
+          <Rive
+            ref={riveAnimationRef}
+            resourceName="water_cup"
+            style={{
+              flex: 1,
+              width: "100%",
+            }}
+          />
+          <RNStyled.TouchableOpacity
+            bg="$show"
+            px="$3"
+            py="$2"
+            rounded="$full"
+            minW="30%"
+            items="center"
+            onPress={handleChangeAmount}
+          >
+            <Typography.Title color="$black">BEBER</Typography.Title>
+          </RNStyled.TouchableOpacity>
+        </View>
       </StyledSafeAreaView>
     </LinearGradient>
   )
