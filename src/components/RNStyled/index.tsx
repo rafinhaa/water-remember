66cd78d3ff2e078ca11247e1cec07f18b2b8d84a
Rafinhaa <rafinhaa@me.com>
2025-04-25 14:56:54 -0300

    feat: components RNStyled

---
+++ src/components/RNStyled/index.tsx
@@ -0,0 +1,6 @@
+import { styled } from "@tamagui/core"
+import { TouchableOpacity as RNTouchableOpacity } from "react-native"
+
+const TouchableOpacity = styled(RNTouchableOpacity)
+
+export default { TouchableOpacity }
