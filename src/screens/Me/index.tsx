5727f5a1a24610a895875933bca9fa8da53f6b52
Rafinhaa <rafinhaa@me.com>
2025-04-25 16:11:50 -0300

    feat: screen Me

--- src/screens/Me/index.tsx
+++ src/screens/Me/index.tsx
@@ -1,9 +1,74 @@
-import { Text, View } from "@tamagui/core"
+import { getTokens, View } from "@tamagui/core"
+import { useTranslation } from "react-i18next"
+import { Switch } from "react-native"
+
+import {
+  Icon,
+  RNStyled,
+  Settings,
+  StyledSafeAreaView,
+  Typography,
+} from "@/components"
+import { useCustomTabBarHeight } from "@/hooks/useCustomTabBarHeight"
+
+import { InfoTotal } from "./components"
 
 export const Me = () => {
+  const { t } = useTranslation()
+  const { distanceToBottom } = useCustomTabBarHeight()
+  const tokens = getTokens()
+
   return (
-    <View>
-      <Text>Me</Text>
-    </View>
+    <StyledSafeAreaView>
+      <RNStyled.ScrollView
+        contentContainerStyle={{
+          grow: 1,
+          px: "$4",
+          gap: "$8",
+          pb: distanceToBottom + tokens.size.$4.val,
+        }}
+      >
+        <View flexDirection="row" gap="$4">
+          <InfoTotal
+            icon="Drop"
+            title="0 ml"
+            description={t("me.totalIngested")}
+          />
+
+          <InfoTotal
+            icon="Trophy"
+            title="0 dias"
+            description={t("me.totalRealized")}
+          />
+        </View>
+
+        <Settings.Section title={t("me.reminderSetup")}>
+          <Settings.Item
+            icon="Bell"
+            title={t("me.reminders")}
+            description={t("me.enableNotifications")}
+          />
+          <Settings.Item
+            icon="Trophy"
+            title={t("me.disableNotifications")}
+            rightAction={<Switch />}
+          />
+        </Settings.Section>
+
+        <Settings.Section title={t("me.toDrink")}>
+          <Settings.Item
+            icon="Drop"
+            title={t("me.dailyGoal")}
+            rightAction={
+              <View flexDirection="row" items="center" gap="$2">
+                <Typography.Title size="sm">0 ml</Typography.Title>
+                <Icon name="CaretRight" color="$sky300" />
+              </View>
+            }
+          />
+          <Settings.Item icon="User" title={t("me.genderAndWeight")} />
+        </Settings.Section>
+      </RNStyled.ScrollView>
+    </StyledSafeAreaView>
   )
 }
